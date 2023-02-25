24e21cc1d41bc0343d722a8c3f6d4452a9a83b3d
SJohnRose <shamini.johnrose@gmail.com>
2023-02-25 22:54:42 +1100

    created queries.js file to store all queries

--- index.js
+++ index.js
@@ -1,86 +1,89 @@
-//  import the inquirer, MySql2 and console.table packages
+//  import the inquirer and console.table packages
 const inquirer = require('inquirer');
-//const mysql = require('mysql2');
 const tableData = require('console.table');
-const db = require('./src/dbConnection');
 
+const db = require('./src/dbConnection');
 const newEmployeeQuestions = require("./src/newEmployeeQuestions");
+const Query = require('./src/queries') ;
 
 const { version } = require('os');
 
-
 const choiceList = ['View all employees', 'Add Employee', 'Update Employee Role', 'View all roles', 'Add Role', 'View all departments', 'Quit'];
+var selection;
+var QueryObj; 
 
 function getAllRoles() {
   var roles = db.query('SELECT title FROM role', function (err, results) {
     console.log(results);
   });
-  
   return roles;
 }
 
 async function askQuestions() {
   while(true) {
-        var selection = await inquirer.prompt([
-        {
-            name: 'userChoice',
-            message: 'What would you like to do?',
-            type: 'list',
-            choices: choiceList,
-        }
-        ]);
-        switch(selection.userChoice) {
-          case choiceList[0]: console.log(choiceList[0] + ' selected');
-                              db.query('SELECT * FROM employee', function (err, results) {
-                                console.log('\n');
-                                console.table(results);
-                              });
-                              break;
-          case choiceList[1]: console.log(choiceList[1] + ' selected');
-                              var newEmployee = await inquirer.prompt(newEmployeeQuestions);
-                              var queryStr = `INSERT INTO employee(first_name, last_name, role_id, manager_id) 
-                                              VALUES ('${newEmployee.firstName}', '${newEmployee.lastName}', ${newEmployee.role_id}, '${newEmployee.manager_id}');`;
-                              db.execute(queryStr, function (err, results) {
-                                  console.log('\n');
-                                  
-                              });               
-                              break;
-          case choiceList[2]: console.log(choiceList[2] + ' selected');
-                              var updateEmployee = await inquirer.prompt([
-                              {
-                                name: 'updateName',
-                                message :"What is the employee's name to update?",
-                              },
-                              {
-                                name: 'newRole',
-                                message : "What is the new role?",
-                                choices : getAllRoles(),
-                              },
-                              ]);
-                              db.query('UPDATE employee SET role_id = ', function (err, results) {
-                                console.log('\n');
-                                console.table(results);
-                              });
-                              break;
-          case choiceList[3]: console.log(choiceList[3] + ' selected');
-                              db.query('SELECT * FROM role', function (err, results) {
-                                console.log('\n');
-                                console.table(results);
-                              });
-                              break;
-          case choiceList[4]: console.log(choiceList[4] + ' selected');
-                              break;
-          case choiceList[5]: console.log(choiceList[5] + ' selected');
-                              db.query('SELECT * FROM department', function (err, results) {
-                                console.log('\n');
-                                console.table(results);
-                              });
-                              break;
-          case choiceList[6]: process.exit(0);
-      }      
+    selection = await inquirer.prompt([
+    {
+      name: 'userChoice',
+      message: 'What would you like to do?',
+      type: 'list',
+      choices: choiceList,
     }
+    ]);
+    switch(selection.userChoice) {
+      case choiceList[0]: console.log(choiceList[0] + ' selected');
+                          var queryStr = 'SELECT * FROM employee;';
+                          runQuery(queryStr);
+                          break;
+      case choiceList[1]: console.log(choiceList[1] + ' selected');
+                          var newEmployee = await inquirer.prompt(newEmployeeQuestions);
+                          QueryObj = new Query(newEmployee);
+                          var queryStr = QueryObj.addEmployee();
+                          // var queryStr = `INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+                          // VALUES ('${newEmployee.firstName}', '${newEmployee.lastName}', ${newEmployee.role_id}, '${newEmployee.manager_id}');`;;
+                          db.execute(queryStr, function (err, results) {
+                            console.log('\n');
+                          });               
+                          break;
+      case choiceList[2]: console.log(choiceList[2] + ' selected');
+                          var updateEmployee = await inquirer.prompt([
+                          {
+                            name: 'firstName',
+                            message :"What is the employee's first name?",
+                          },
+                          {
+                          name: 'lastName',
+                          message :"What is the employee's last name?",
+                          },
+                          {
+                            name: 'newRole',
+                            message : "What is the new role?",
+                            choices : getAllRoles(),
+                          },
+                          ]);
+                          var queryStr = `UPDATE employee SET role_id = '${updateEmployee.choices}' where first_name = '${updateEmployee.firstName}' 
+                          and last_name = '${updateEmployee.lastName}';`
+                          runQuery(queryStr);
+                          break;
+      case choiceList[3]: console.log(choiceList[3] + ' selected');
+                          var queryStr = 'SELECT * FROM role';
+                          runQuery(queryStr);
+                          break;
+      case choiceList[4]: console.log(choiceList[4] + ' selected');
+                          break;
+      case choiceList[5]: console.log(choiceList[5] + ' selected');
+                          var queryStr = 'SELECT * FROM department';
+                          runQuery(queryStr);
+                          break;
+      case choiceList[6]: process.exit(0);
+    }      
+  }
 }
 
-
+function runQuery(query) {
+  db.query(query, function (err, results) {
+    console.log('\n');
+    console.table(results);
+  });
+}
 
 askQuestions();
--- src/queries.js
+++ src/queries.js
@@ -1,1 +1,35 @@
+class Query {
+    constructor(answers) {
+        this.answers = answers;
+        
+    }
+    viewEmployees() {
+        var queryStr = 'SELECT * FROM employee;';
+        return queryStr;
+    }
 
+    addEmployee() {
+        var queryStr = `INSERT INTO employee(first_name, last_name, role_id, manager_id) 
+                          VALUES ('${this.answers.firstName}', 
+                          '${this.answers.lastName}', ${this.answers.role_id}, '${this.answers.manager_id}');`;
+        return queryStr;
+    }
+
+    upDateEmployeeRole() {
+
+    }
+
+    viewRoles() {
+
+    }
+
+    addRole() {
+
+    }
+
+    viewDepartments() {
+
+    }
+}
+
+module.exports = Query
